89ca51ddfa6589c30b54bf85ae1f12ff4b0c32ab
bipproduction <bip.production.js@gmail.com>
2023-08-21 14:01:54 +0800

    meniadakan env

--- http/login.sh
+++
@@ -1,6 +0,0 @@
-. .env.http
-
-curl -c "cookies.txt" \
--i -H 'Content-Type: application/json' \
--d '{"name": "malik", "password": "makuro_123"}' \
--X POST "$HTTP_URL"/api/auth/login
--- http/upl.sh
+++
@@ -1,5 +0,0 @@
-. .env.http
-
-curl  -X POST "$HTTP_URL"/api/file/upl \
--b "$(cat cookies.txt)" \
--i -F "file=@./img.png"
